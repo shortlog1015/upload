e61c9c31a3b64031a0d4b66bcbe9870e6fde7eed
ryang <ryang@evolveconsulting.com.hk>
2019-06-24 18:25:46 +0800

    update

--- client/main.go
+++ client/main.go
@@ -1,8 +1,79 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net"
+	"net/http"
+	"os"
+	"time"
+)
+
+const URL = "http://139.224.22.239/upload/trans"
+const TestURL = "http://localhost:12700/trans?name=%s&off=%d"
+
 func main() {
+	file, err := os.Open("test.txt")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer file.Close()
+	client := initClient()
+	var endErr error
+	for i := 0; endErr == nil; i++ {
+		content := make([]byte, 8<<24)
+		offset := (8 << 24) * int64(i)
+		n, err := file.ReadAt(content, offset)
+		if err != nil {
+			if err == io.EOF {
+				endErr = err
+			} else {
+				log.Println(err)
+				return
+			}
+		}
+		log.Println(n)
+
+		body := &bytes.Buffer{}
+		writer := multipart.NewWriter(body)
+		fw, err := writer.CreateFormFile("content", file.Name())
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		_, err = io.Copy(fw, bytes.NewReader(content))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		writer.Close()
+		req, err := http.NewRequest(http.MethodPost, fmt.Sprintf(TestURL, file.Name(), offset), body)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer resp.Body.Close()
+		res, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		log.Println("resp:", string(res))
+	}
 }
 
-func uploadContent() {
-
+func initClient() *http.Client {
+	dialer := net.Dialer{Timeout: 5 * time.Second, KeepAlive: 10 * time.Second}
+	return &http.Client{Transport: &http.Transport{DialContext: dialer.DialContext}, Timeout: 7 * time.Second}
 }
--- server/main.go
+++ server/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"io"
+	"log"
 	"mime/multipart"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -18,6 +20,10 @@ func main() {
 }
 
 func getUploadFile(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "wrong method", http.StatusNotAcceptable)
+		return
+	}
 	if err := r.ParseMultipartForm(8 << 24); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
@@ -58,5 +64,44 @@ func getMultiPartFiles(r *http.Request, key string) ([]*multipart.FileHeader, er
 }
 
 func getTransFile(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method != http.MethodPost {
+		http.Error(w, "wrong method", http.StatusNotAcceptable)
+		return
+	}
+	if err := r.ParseMultipartForm(8 << 24); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	query := r.URL.Query()
+	name := query.Get("name")
+	off, _ := strconv.Atoi(query.Get("off"))
+	log.Println(name, off)
+	if name == "" {
+		http.Error(w, "no name", http.StatusBadRequest)
+		return
+	}
+	fileHeaders, err := getMultiPartFiles(r, "content")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	for _, fh := range fileHeaders {
+		srcFile, err := fh.Open()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusResetContent)
+			return
+		}
+		defer srcFile.Close()
+		dstFile, err := os.Create("./files/" + fh.Filename)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusResetContent)
+			return
+		}
+		defer dstFile.Close()
+		if _, err := io.Copy(dstFile, srcFile); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte("files ok"))
+	}
 }
